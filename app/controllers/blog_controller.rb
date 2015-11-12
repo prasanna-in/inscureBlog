0d03be1cea7258d513c2b8286959ee5c93186839
Shifu <prasanna.in@gmail.com>
2015-11-12 17:41:43 +0800

    Added New Blog Feature

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,11 +2,28 @@ class BlogController < ApplicationController
   def index
     @blogs = Blog.all
   end
-  def new
 
+  def new
+    # @blog = Blog.new
   end
+
   def show
     @blog = Blog.find(params[:id])
     @links = @blog.links
   end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to @blog
+    else
+      render 'new'
+    end
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title,:body,:links)
+  end
+
 end
