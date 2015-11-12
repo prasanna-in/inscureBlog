37b9ddb4ebef908e4af461bfb8de3ecf29fbe41b
Alex Lawrence <pair@momdev.com>
2015-11-12 22:42:32 +0800

    Tech | added secret yml config and change default server port to 8000.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module InsecureBlog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.secret_key_base = YAML.load(File.open("#{Rails.root}/config/secrets.yml"))[Rails.env]['secret_key_base']
   end
 end
